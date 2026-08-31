7e9f1006368059632301af7e3ded35a2e7ab3eb6
vijaysen964 <vijaysen964@gmail.com>
2026-08-31 17:49:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "christ-baptist-church-v1";
+const CACHE_NAME = "christ-baptist-church-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
